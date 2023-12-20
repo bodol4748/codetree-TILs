5a1b6f6d4a46b0f6e5ff23179e662626987b2bca
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2023-12-20 10:45:27 +0900

    Title: 금 채굴하기, Time: 234ms, Memory: 11MB, Status: Passed - CodeTree

--- 231220/금 채굴하기/gold-mining.java
+++ 231220/금 채굴하기/gold-mining.java
@@ -6,34 +6,19 @@ public class Main {
     public static int n, m;
     public static int[][] grid = new int[MAX_NUM][MAX_NUM];
     
+    // 주어진 k에 대하여 마름모의 넓이를 반환합니다.
     public static int getArea(int k) {
         return k * k + (k+1) * (k+1); 
     }
     
-    public static boolean inRange(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < n;
-    }
-    
-    // 주어진 k에 대하여 모서리에서 채굴 가능한 금의 개수를 반환합니다.
-    public static int getNumOfGoldInBorder(int row, int col, int k) {
+    // 주어진 k에 대하여 채굴 가능한 금의 개수를 반환합니다.
+    public static int getNumOfGold(int row, int col, int k) {
         int numOfGold = 0;
-        int[] dx = new int[]{1, 1, -1, -1};
-        int[] dy = new int[]{-1, 1, 1, -1}; // 방향에 따라 바뀌는 x와 y의 변화량을 정의합니다.
     
-        if(k == 0)
-            return grid[row][col];
-    
-        int currX = row - k, currY = col; // 순회 시작점 설정
-    
-        for(int currDir = 0; currDir < 4; currDir++) {
-            for(int step = 0; step < k; step++) {
-                if(inRange(currX, currY)) {
-                    numOfGold += grid[currX][currY];
-                }
-                currX = currX + dx[currDir];
-                currY = currY + dy[currDir];
-            }
-        }
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(Math.abs(row - i) + Math.abs(col - j) <= k)
+                    numOfGold += grid[i][j];
     
         return numOfGold;
     }
@@ -42,21 +27,20 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int maxGold = 0;
 
+        // 입력:
         n = sc.nextInt();
         m = sc.nextInt();
         for(int row = 0; row < n; row++)
             for(int col = 0; col < n; col++)
                 grid[row][col] = sc.nextInt();
 
-
+        // 격자의 각 위치가 마름모의 중앙일 때 채굴 가능한 금의 개수를 구합니다.
         for(int row = 0; row < n; row++) {
             for(int col = 0; col < n; col++) {
-                int numOfGold = 0;
                 for(int k = 0; k <= 2 * (n-1); k++) {
-                    // 이전 k까지 구한 금의 개수에
-                    // 해당 k의 모서리에 존재하는 금의 개수를 더해줍니다.
-                    numOfGold += getNumOfGoldInBorder(row, col, k);
+                    int numOfGold = getNumOfGold(row, col, k);
 
+                    // 손해를 보지 않으면서 채굴할 수 있는 최대 금의 개수를 저장합니다.
                     if(numOfGold * m >= getArea(k))
                         maxGold = Math.max(maxGold, numOfGold);
                 }
