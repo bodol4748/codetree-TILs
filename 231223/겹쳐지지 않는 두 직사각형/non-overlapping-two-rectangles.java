5db2d3e5fb31406e9bc94e40c9f97fdd5e334fae
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2023-12-23 14:44:18 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 149ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231223/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+    private static int[][] arr;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        arr = new int[n][m];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        } // array에 입력값 넣기
+
+        for(int y1 = 0; y1 < n; y1++) {
+            for(int x1 = 0; x1 < m; x1++) {
+                for(int y2 = y1; y2 < n; y2++) {
+                    for(int x2 = x1; x2 < m; x2++) {
+                        // 첫번째 직사각형 꼭짓점 2개 선택
+                        int rec1 = calc(y1, x1, y2, x2);
+                        for(int y3 = y1; y3 < n; y3++) {
+                            for(int x3 = x1; x3 < m; x3++) {
+                                for(int y4 = y3; y4 < n; y4++) {
+                                    for(int x4 = x3; x4 < m; x4++) {
+                                        if(x3 <= x2 && x3 >= x1 && y3 >= y1 && y3 <= y2) {
+                                            continue;
+                                        }
+
+                                        int rec2 = calc(y3, x3, y4, x4);
+
+                                        max = max < rec1 + rec2 ? rec1 + rec2 : max;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+
+    // 직사각형 꼭짓점(대각선방향) 2개 받아와 내부 값 합산
+    private static int calc(int y1, int x1, int y2, int x2) {
+        int sum = 0;
+        for(int y = y1; y <= y2; y++) {
+            for(int x = x1; x <= x2; x++) {
+                sum += arr[y][x];
+            }
+        }
+        return sum;
+    }
+}
